76b8aa639c84b13db2fdc7739b5fcd65559da5d6
Ohazulike Stanley <sohazulike@gmail.com>
2025-09-14 13:32:13 -0700

    add task_2

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,54 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+class DirectorClass implements DirectorInterface {
+  workFromHome(): string {
+    return 'Working from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Getting a coffee break';
+  }
+
+  workDirectorTasks(): string {
+    return 'Getting to director tasks';
+  }
+}
+
+class TeacherClass implements TeacherInterface {
+  workFromHome(): string {
+    return 'Cannot work from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Cannot have a break';
+  }
+
+  workTeacherTasks(): string {
+    return 'Getting to work';
+  }
+}
+
+
+function createEmployee(salary: number | string): DirectorClass | TeacherClass {
+  // If salary is a number and less than 500, return Teacher
+  if (typeof salary === 'number' && salary < 500) {
+    return new TeacherClass();
+  }
+  // Otherwise, return Director
+  return new DirectorClass();
+}
+
+// Example usage
+console.log(createEmployee(200));   // -> TeacherClass instance
+console.log(createEmployee(1000));  // -> DirectorClass instance
+console.log(createEmployee('$500')); // -> DirectorClass instance
