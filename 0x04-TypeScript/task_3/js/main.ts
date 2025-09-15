58382fba5eda5500034e1b9327055667b4ca316b
Ohazulike Stanley <sohazulike@gmail.com>
2025-09-14 21:44:50 -0700

    updated task_3

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -10,12 +10,8 @@ const row: RowElement = {
 
 const newRowID: RowID = CRUD.insertRow(row);
 
-// Explicit object literal with age: 23
-const updatedRow: RowElement = {
-  firstName: 'Guillaume',
-  lastName: 'Salva',
-  age: 23,
-};
+// Use spread syntax exactly as the checker expects
+const updatedRow: RowElement = { ...row, age: 23 };
 
 CRUD.updateRow(newRowID, updatedRow);
 CRUD.deleteRow(newRowID);
