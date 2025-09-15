5aa665f944bc081fed00c29443b58c0a184b2700
Ohazulike Stanley <sohazulike@gmail.com>
2025-09-14 21:43:01 -0700

    updated task_3

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,19 +1,19 @@
-// task_3/main.ts
 /// <reference path="./crud.d.ts" />
 
-
-import { RowID, RowElement } from "./interface";
-import * as CRUD from "./crud";
+import { RowID, RowElement } from './interface';
+import * as CRUD from './crud';
 
 const row: RowElement = {
-  firstName: "Guillaume",
-  lastName: "Salva",
+  firstName: 'Guillaume',
+  lastName: 'Salva',
 };
 
 const newRowID: RowID = CRUD.insertRow(row);
 
+// Explicit object literal with age: 23
 const updatedRow: RowElement = {
-  ...row,
+  firstName: 'Guillaume',
+  lastName: 'Salva',
   age: 23,
 };
 
